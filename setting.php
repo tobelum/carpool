5f754c225ee0cf8d250d8418879977671c16bc3c
tobelum <tohbelle@gmail.com>
2016-11-11 03:29:32 +0000

    checking why its not connecting to the database

--- setting.php
+++ setting.php
@@ -1,5 +1,5 @@
 <?php
-define("DB_HOST","localhost");
+define("DB_HOST","52.89.116.249");
 define("DB_USERNAME","tobel.eze-okoli");
 define("DB_PASSWORD","8f7c19e8964a7e09");
 define("DB_NAME","db__tobel_eze-okoli");
